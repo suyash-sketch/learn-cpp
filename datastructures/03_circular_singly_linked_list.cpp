22f04474129409766005a1cf92feebe5de07d74e
Suyash <suyash.mkhandagale@gmail.com>
2026-09-26 22:24:47 +0530

    circular ll: traversal, insert at front, back and at position

--- datastructures/03_circular_singly_linked_list.cpp
+++ datastructures/03_circular_singly_linked_list.cpp
@@ -1,3 +1,4 @@
+#include <future>
 #include <iostream>
 
 using namespace std;
@@ -18,6 +19,88 @@ class CircularSinglyLinkedList{
     void printList(Node *last){
       Node *head = last->next;
       Node* temp = head;
+
+      do {
+        cout << temp->data << " ";
+        temp = temp->next;
+        
+      } while (temp != head);
+      cout << endl;
+    }
+
+    Node *insertAtBeginning(Node *tail, int new_val){
+      Node *newNode = new Node(new_val);
+
+      if (tail == nullptr){
+        newNode->next = newNode;
+        return newNode;
+      }
+
+      newNode->next = tail->next;
+      tail->next = newNode;
+
+      return tail;
+    }
+
+    Node *insertAtEnd(Node *tail, int new_val){
+      Node *newNode = new Node(new_val);
+      if (tail == nullptr){
+        newNode->next = newNode;
+        return newNode;
+      }
+
+      Node *curr = tail;
+      newNode->next = curr->next;
+      curr->next = newNode;
+
+      tail = newNode;
+      return tail;
+    }
+
+    Node *insertAtPosition(Node *tail, int position, int new_val){
+
+      if (tail == nullptr){
+        // if list is empty
+        if (position != 1){
+            cout << "Invalid position" << endl; 
+        }
+
+        // create a new node and make it point to itself
+        Node *newNode = new Node(new_val);
+        newNode->next = newNode;
+        return newNode;
+      }
+
+      Node *newNode = new Node(new_val);
+
+      Node *curr = tail->next;
+
+      if (position == 1){
+        newNode->next = curr;
+        tail->next = newNode;
+
+        return tail;
+      }
+
+      // traverse to position
+      for (int i = 1; i < position - 1; i++){
+        curr = curr->next;
+
+        // if position is out of bounds
+        if (curr == tail->next){
+          cout << "Invalid Position" <<endl;
+          return tail;
+        }
+      }
+
+      // insert new node
+      newNode->next = curr->next;
+      curr->next =  newNode;
+
+      // update tail if the new node is inserted at end
+      if (curr == tail) tail = newNode;
+
+      return tail;
     }
 };
 
@@ -30,5 +113,17 @@ int main(){
   tail->next = head;
 
   CircularSinglyLinkedList cll;
-  cll.printList(head);
+  cll.printList(tail);
+
+  tail = cll.insertAtBeginning(tail, 39);
+  cll.printList(tail);
+
+  tail = cll.insertAtEnd(tail, 303);
+  cll.printList(tail);
+
+  tail = cll.insertAtEnd(tail, 03);
+  cll.printList(tail);
+
+  tail = cll.insertAtPosition(tail, 3, 46);
+  cll.printList(tail);
 }
